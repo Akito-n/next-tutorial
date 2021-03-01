bb7dfb01c583c0a8d8b67fce3db3e8eb5322bb1d
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-01 22:36:33 +0900

    一覧表示できた

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,21 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+// export async function getServerSideProps(context) {
+//   return { props: {} }
+// }
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,6 +24,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
+
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+
         <Link href="/posts/first-post">
           <a>first post</a>
         </Link>
