8025d76e6ac96f8d0e52193de6515928702c258d
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-02 15:31:42 +0900

    design つけた

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -19,9 +20,9 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      {/* <Head>
+      <Head>
         <title>{siteTitle}</title>
-      </Head> */}
+      </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
 
@@ -29,11 +30,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
